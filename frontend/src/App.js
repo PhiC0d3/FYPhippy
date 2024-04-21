f55a63244b021481fbeddb4f75e182738c1622be
Bryan Kyritz <bryan@securemeeting.org>
2024-04-21 15:19:44 -0400

    ye

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,7 +6,17 @@ import { RealtimeLayer } from "mobility-toolbox-js/mapbox";
 import { SearchBox } from "@mapbox/search-js-react";
 import "@mapbox/mapbox-gl-directions/dist/mapbox-gl-directions.css";
 
+import styled from "styled-components";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faWalking } from "@fortawesome/free-solid-svg-icons";
+
 const apiKey = "5cc87b12d7c5370001c1d6553983bc2b94014146b168ea7b563e7b7f";
+
+const StyledSearchBoxContainer = styled.div`
+  && input {
+    height: 60px !important;
+  }
+`;
 
 const App = () => {
   const [viewport, setViewport] = useState({
@@ -23,9 +33,11 @@ const App = () => {
   const geoControlRef = useRef();
   const [simulateWalking, setSimulateWalking] = useState(false);
   const [stepCoordinates, setStepCoordinates] = useState([]);
-
+  const [tripInProgress, setTripInProgress] = useState(false);
   const [lastUpdatedStepIndex, setLastUpdatedStepIndex] = useState(-1); // Add this state to track the last updated step index
-
+  const [tripDetails, setTripDetails] = useState(null);
+  const [realTimeUpdateTripDetails, setRealTimeUpdateTripDetails] =
+    useState(null);
   const mapboxAccessToken =
     "pk.eyJ1Ijoia3lyaXR6YiIsImEiOiJjbHVoZDJpZTUyazFlMm1wNmtqeWFwbzIzIn0.dxpXiLCDsPtNDDbKx2ksLA"; // Replace with your Mapbox access token
 
@@ -89,53 +101,63 @@ const App = () => {
     updateUserLocation();
   };
 
+  async function updateTripDetails(location, initial = true) {
+    const directionsService = new window.google.maps.DirectionsService();
+    const directionsRenderer = new window.google.maps.DirectionsRenderer();
+
+    const request = {
+      origin: { lat: viewport.latitude, lng: viewport.longitude },
+      destination: { lat: location[1], lng: location[0] },
+      travelMode: window.google.maps.TravelMode.TRANSIT,
+    };
+
+    directionsService.route(request, (result, status) => {
+      console.log(result);
+      if (initial) {
+        setTripDetails(result);
+        setRealTimeUpdateTripDetails(result);
+      } else {
+        setRealTimeUpdateTripDetails(result);
+      }
+      if (status === window.google.maps.DirectionsStatus.OK) {
+        const route = result.routes[0].overview_path.map((point) => [
+          point.lng(),
+          point.lat(),
+        ]);
+        setRoute(route);
+
+        const instructions = result.routes[0].legs[0].steps.map(
+          (step) => step.instructions
+        );
+
+        console.log(instructions);
+        setInstructions(instructions);
+
+        const coordinates = result.routes[0].legs[0].steps.map(
+          (step) => step.end_location
+        );
+        const stepCoords = coordinates.map((coord) => [
+          coord.lng(),
+          coord.lat(),
+        ]);
+        setStepCoordinates(stepCoords);
+      } else {
+        console.error("Error fetching directions:", status);
+      }
+    });
+  }
+
   async function pickedDestination(e) {
     const location = e.features[0].geometry.coordinates;
     setDestination(location);
 
     if (viewport.latitude && viewport.longitude && location) {
-      const directionsService = new window.google.maps.DirectionsService();
-      const directionsRenderer = new window.google.maps.DirectionsRenderer();
-
-      const request = {
-        origin: { lat: viewport.latitude, lng: viewport.longitude },
-        destination: { lat: location[1], lng: location[0] },
-        travelMode: window.google.maps.TravelMode.TRANSIT,
-      };
-
-      directionsService.route(request, (result, status) => {
-        console.log(result);
-        if (status === window.google.maps.DirectionsStatus.OK) {
-          const route = result.routes[0].overview_path.map((point) => [
-            point.lng(),
-            point.lat(),
-          ]);
-          setRoute(route);
-
-          const instructions = result.routes[0].legs[0].steps.map(
-            (step) => step.instructions
-          );
-
-          console.log(instructions);
-          setInstructions(instructions);
-
-          const coordinates = result.routes[0].legs[0].steps.map(
-            (step) => step.end_location
-          );
-          const stepCoords = coordinates.map((coord) => [
-            coord.lng(),
-            coord.lat(),
-          ]);
-          setStepCoordinates(stepCoords);
-        } else {
-          console.error("Error fetching directions:", status);
-        }
-      });
+      updateTripDetails(location);
     }
   }
 
   const updateCurrentStep = () => {
-    console.log("Updatee current step");
+    console.log("Update current step");
     if (stepCoordinates.length > 0) {
       const userLocation = [viewport.longitude, viewport.latitude];
 
@@ -152,36 +174,19 @@ const App = () => {
       }
 
       // If the user is within a certain distance threshold of the closest step, and the closest step is beyond the last updated step, update the current step
-      if (closestDistance < 0.03 && closestStepIndex > lastUpdatedStepIndex) {
-        console.log(closestDistance);
+      if (closestDistance < 0.01 && closestStepIndex > lastUpdatedStepIndex) {
         closestStepIndex = closestStepIndex + 1;
         setCurrentStep(closestStepIndex);
         setLastUpdatedStepIndex(closestStepIndex); // Update the last updated step index
       }
+      // Fly the map to the user's current location
+      const map = mapRef.current.getMap();
+      map.flyTo({
+        center: userLocation,
+        // this animation is considered essential with respect to prefers-reduced-motion
+      });
 
-      if (destination) {
-        const directionsService = new window.google.maps.DirectionsService();
-        const directionsRenderer = new window.google.maps.DirectionsRenderer();
-
-        const request = {
-          origin: { lat: viewport.latitude, lng: viewport.longitude },
-          destination: { lat: destination[1], lng: destination[0] },
-          travelMode: window.google.maps.TravelMode.TRANSIT,
-        };
-
-        directionsService.route(request, (result, status) => {
-          console.log(result);
-          if (status === window.google.maps.DirectionsStatus.OK) {
-            const route = result.routes[0].overview_path.map((point) => [
-              point.lng(),
-              point.lat(),
-            ]);
-            setRoute(route);
-          } else {
-            console.error("Error fetching directions:", status);
-          }
-        });
-      }
+      updateTripDetails(destination, false);
     }
   };
 
@@ -224,9 +229,301 @@ const App = () => {
           clearInterval(intervalId);
           setSimulateWalking(false);
         }
-      }, 1000);
+      }, 500);
     }
   };
+
+  function renderBottomPanel() {
+    console.log(tripDetails);
+    if (route) {
+      if (tripInProgress) {
+        return (
+          <>
+            <div
+              style={{
+                position: "absolute",
+                top: "0px",
+                height: "125px",
+                zIndex: "100",
+                width: "100%",
+                paddingTop: "10px",
+                backgroundColor: "rgb(30,30,30)",
+              }}
+            >
+              <p
+                style={{
+                  fontSize: "30px",
+                  fontWeight: "600",
+                  color: "white",
+                  paddingLeft: "5vw",
+                }}
+                dangerouslySetInnerHTML={{
+                  __html: instructions[currentStep]
+                    ? instructions[currentStep]
+                    : "You've arrived at your destination",
+                }}
+              ></p>
+              {instructions[currentStep + 1] && (
+                <div
+                  style={{
+                    backgroundColor: "rgb(60,60,60)",
+                    height: "50px",
+                    display: "flex",
+                    alignItems: "center",
+                    paddingLeft: "5vw",
+                  }}
+                >
+                  <p
+                    style={{
+                      fontSize: "20px",
+                      fontWeight: "600",
+                      color: "white",
+                    }}
+                    dangerouslySetInnerHTML={{
+                      __html: instructions[currentStep + 1],
+                    }}
+                  ></p>
+                </div>
+              )}
+            </div>
+
+            {instructions[currentStep] && (
+              <div
+                style={{
+                  position: "absolute",
+                  bottom: "0px",
+                  height: "75px",
+                  zIndex: "100",
+                  width: "100%",
+                  paddingTop: "10px",
+                  backgroundColor: "white",
+                }}
+              >
+                <div
+                  style={{
+                    display: "flex",
+                    flexDirection: "row",
+                    justifyContent: "space-between",
+                    paddingLeft: "5vw",
+                    paddingRight: "5vw",
+                  }}
+                >
+                  <div
+                    style={{
+                      display: "flex",
+                      flexDirection: "row",
+                      alignItems: "center",
+                      gap: "30px",
+                    }}
+                  >
+                    <div
+                      style={{
+                        display: "flex",
+                        flexDirection: "column",
+                        alignItems: "center",
+                      }}
+                    >
+                      <p
+                        style={{
+                          margin: "0px",
+                          fontSize: "24px",
+                          fontWeight: "600",
+                        }}
+                      >
+                        {
+                          realTimeUpdateTripDetails.routes[0].legs[0]
+                            .arrival_time.text
+                        }
+                      </p>
+                      <p style={{ margin: "0px" }}>arrival</p>
+                    </div>
+                    <div
+                      style={{
+                        display: "flex",
+                        flexDirection: "column",
+                        alignItems: "center",
+                      }}
+                    >
+                      <p
+                        style={{
+                          margin: "0px",
+                          fontSize: "24px",
+                          fontWeight: "600",
+                        }}
+                      >
+                        {(
+                          realTimeUpdateTripDetails.routes[0].legs[0].duration
+                            .value / 60
+                        ).toFixed(0)}
+                      </p>
+                      <p style={{ margin: "0px" }}>min</p>
+                    </div>
+                    <div
+                      style={{
+                        display: "flex",
+                        flexDirection: "column",
+                        alignItems: "center",
+                      }}
+                    >
+                      <p
+                        style={{
+                          margin: "0px",
+                          fontSize: "24px",
+                          fontWeight: "600",
+                        }}
+                      >
+                        {(
+                          realTimeUpdateTripDetails.routes[0].legs[0].distance
+                            .value / 1609
+                        ).toFixed(1)}
+                      </p>
+                      <p style={{ margin: "0px" }}>mi</p>
+                    </div>
+                  </div>
+                  <button
+                    onClick={() => {
+                      setSimulateWalking(
+                        (prevSimulateWalking) => !prevSimulateWalking
+                      );
+                      if (!simulateWalking) {
+                        simulateWalkingFunction();
+                      }
+                    }}
+                    style={{
+                      height: "50px",
+                      width: "50px",
+                      backgroundColor: !simulateWalking ? "limegreen" : "red",
+                      outline: "1px white solid",
+                    }}
+                  >
+                    <FontAwesomeIcon
+                      icon={faWalking}
+                      style={{ fontSize: "30", color: "white" }}
+                    />
+                  </button>
+                </div>
+              </div>
+            )}
+          </>
+        );
+      } else {
+        // Additional view when route is true but tripInProgress is false
+        return (
+          <div
+            style={{
+              position: "absolute",
+              bottom: "0px",
+              height: "100px",
+              zIndex: "100",
+              width: "100%",
+              backgroundColor: "white",
+              display: "flex",
+              flexDirection: "row",
+              alignItems: "center",
+              justifyContent: "space-between",
+            }}
+          >
+            <div
+              style={{
+                paddingLeft: "10vw",
+              }}
+            >
+              <p
+                style={{
+                  fontSize: "20px",
+                  marginBottom: "0px",
+                  fontWeight: "700",
+                }}
+              >
+                {tripDetails.routes[0].legs[0].duration.text}
+              </p>
+              <p
+                style={{
+                  fontSize: "12px",
+                  marginTop: "5px",
+                  color: "rgb(75,75,75)",
+                }}
+              >
+                {tripDetails.routes[0].legs[0].distance.text}
+              </p>
+            </div>
+            <div style={{ paddingRight: "10vw" }}>
+              <button
+                style={{
+                  backgroundColor: "limegreen",
+                  color: "white",
+                  height: "75px",
+                  width: "75px",
+                  borderRadius: "15px",
+                  border: "none",
+                  fontSize: "22px",
+                  fontWeight: "700",
+                  cursor: "pointer",
+                }}
+                onClick={() => {
+                  const userLocation = [viewport.longitude, viewport.latitude];
+
+                  const mapInstance = mapRef.current
+                    ? mapRef.current.getMap()
+                    : null;
+
+                  if (mapInstance) {
+                    mapInstance.flyTo({ center: userLocation });
+
+                    // Update the viewport state to trigger a re-render
+                    // You might want to adjust the zoom level or other properties as needed
+                    setViewport((prevViewport) => ({
+                      ...prevViewport,
+                      longitude: userLocation[0],
+                      latitude: userLocation[1],
+                      // Optionally adjust zoom here if needed
+                    }));
+                  }
+                  setTripInProgress(true);
+                }}
+              >
+                Go
+              </button>
+            </div>
+          </div>
+        );
+      }
+    } else {
+      return (
+        <div
+          style={{
+            position: "absolute",
+            bottom: "0px",
+            zIndex: "100",
+            width: "100%",
+            paddingTop: "10px",
+          }}
+        >
+          <StyledSearchBoxContainer>
+            <SearchBox
+              accessToken={mapboxAccessToken}
+              options={{
+                language: "en",
+              }}
+              popoverOptions={{
+                placement: "top-start",
+                flip: true,
+                offset: 5,
+              }}
+              placeholder="Search"
+              value={searchbartext}
+              onChange={(e) => {
+                setSearchbarText(e);
+              }}
+              onRetrieve={pickedDestination}
+              marker={true}
+              map={mapRef.current && mapRef.current.getMap()}
+            />
+          </StyledSearchBoxContainer>
+        </div>
+      );
+    }
+  }
 
   return (
     <>
@@ -243,73 +540,8 @@ const App = () => {
         `}
       </style>
       <style>{pulseAnimation}</style>
-      <div>
-        {route ? (
-          <div
-            style={{
-              position: "absolute",
-              bottom: "0px",
-              height: "200px",
-              zIndex: "100",
-              width: "100%",
-              paddingTop: "10px",
-              backgroundColor: "white",
-            }}
-          >
-            <h3>Current Step:</h3>
-            <p
-              dangerouslySetInnerHTML={{ __html: instructions[currentStep] }}
-            ></p>
-            <button
-              onClick={() => {
-                setSimulateWalking(
-                  (prevSimulateWalking) => !prevSimulateWalking
-                );
-                if (!simulateWalking) {
-                  simulateWalkingFunction();
-                }
-              }}
-            >
-              {simulateWalking ? "Stop Simulation" : "Simulate Walking"}
-            </button>
-          </div>
-        ) : (
-          mapRef && (
-            <div
-              style={{
-                position: "absolute",
-                bottom: "0px",
-                height: "75px",
-                zIndex: "100",
-                width: "100%",
-                paddingTop: "10px",
-              }}
-            >
-              <SearchBox
-                accessToken={mapboxAccessToken}
-                options={{
-                  language: "en",
-                }}
-                popoverOptions={{
-                  placement: "top-start",
-                  flip: true,
-                  offset: 5,
-                }}
-                placeholder="Search"
-                value={searchbartext}
-                onChange={(e) => {
-                  console.log(e);
-                  setSearchbarText(e);
-                }}
-                onRetrieve={pickedDestination}
-                style={{ height: "75px" }}
-                marker={true}
-                map={mapRef.current && mapRef.current.getMap()}
-              ></SearchBox>
-            </div>
-          )
-        )}
-      </div>
+
+      <div>{renderBottomPanel()}</div>
       <Map
         ref={mapRef}
         mapLib={import("mapbox-gl")}
